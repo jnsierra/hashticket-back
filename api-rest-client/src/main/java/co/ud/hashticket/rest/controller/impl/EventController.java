e9bd4b985e0c5c1f2f01bbe5599e14d64f82382f
Jesus Nicolas Sierra Chaparro <nicolas@iMac-de-Jesus.local>
2023-02-18 13:27:01 -0500

    Adicion de controlador de eventos

---
+++ api-rest-client/src/main/java/co/ud/hashticket/rest/controller/impl/EventController.java
@@ -0,0 +1,17 @@
+package co.ud.hashticket.rest.controller.impl;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v.1/event")
+public class EventController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Este es el servicio de eventos";
+    }
+}
+
