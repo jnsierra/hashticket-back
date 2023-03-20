a1cf797be511534eb9bd24d8d82121f7cdc6cd8c
Jesus Nicolas Sierra Chaparro <nicolas@iMac-de-Jesus.local>
2023-03-20 15:38:54 -0500

    Ajuste de url

--- api-rest-client/src/main/java/co/ud/hashticket/client/EventImagesClient.java
+++ api-rest-client/src/main/java/co/ud/hashticket/client/EventImagesClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "eventImagesClient", url = "${endpoint.ms-data-access.protocol}:${endpoint.ms-data-access.host}//:${endpoint.ms-data-access.port}/api-acceso-datos/v.1/event_images")
+@FeignClient(name = "eventImagesClient", url = "${endpoint.ms-data-access.protocol}://${endpoint.ms-data-access.host}:${endpoint.ms-data-access.port}/api-acceso-datos/v.1/event_images")
 public interface EventImagesClient {
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     EventImagesDto save(@RequestBody EventImagesDto eventImagesDto);
--- api-rest-client/src/main/java/co/ud/hashticket/client/ZoneConfigEventClient.java
+++ api-rest-client/src/main/java/co/ud/hashticket/client/ZoneConfigEventClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Set;
 
-@FeignClient(name = "ZoneConfigEventClient", url = "${endpoint.ms-data-access.protocol}:${endpoint.ms-data-access.host}//:${endpoint.ms-data-access.port}/api-acceso-datos/v.1/zone_config_event/")
+@FeignClient(name = "ZoneConfigEventClient", url = "${endpoint.ms-data-access.protocol}://${endpoint.ms-data-access.host}:${endpoint.ms-data-access.port}/api-acceso-datos/v.1/zone_config_event/")
 public interface ZoneConfigEventClient {
     @GetMapping(value = "/event/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     Set<ZoneConfigEventDto> getByIdEvent(@PathVariable(value = "id")Long idEvent);
