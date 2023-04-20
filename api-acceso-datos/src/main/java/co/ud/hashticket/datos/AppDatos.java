e93463fd4a910e3dba49f045668c010e37b889dd
Jesus Nicolas Sierra Chaparro <nicolas@iMac-de-Jesus.local>
2023-04-20 15:47:08 -0500

    Ajuste para logs

--- api-acceso-datos/src/main/java/co/ud/hashticket/datos/AppDatos.java
+++ api-acceso-datos/src/main/java/co/ud/hashticket/datos/AppDatos.java
@@ -1,5 +1,7 @@
 package co.ud.hashticket.datos;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +16,13 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("co.ud")
 public class AppDatos {
 
+    private static final Logger LOGGER = LogManager.getLogger(AppDatos.class);
+
     public static void main(String... args) {
         final ConfigurableApplicationContext applicationContext = SpringApplication.run(AppDatos.class);
         LoggerFactory.getLogger(AppDatos.class).info("Los beans son {} ", applicationContext.getBeanDefinitionNames().length);
+        LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
     }
 }
