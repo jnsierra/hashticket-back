9fddd7b50ef1fc70822a02897af0e25356b5515e
Jesus Nicolas Sierra Chaparro <nicolas@iMac-de-Jesus.local>
2023-05-05 16:26:53 -0500

    Ajustes

--- api-acceso-datos/src/main/java/co/ud/hashticket/datos/service/impl/ZoneConfigEventServiceImpl.java
+++ api-acceso-datos/src/main/java/co/ud/hashticket/datos/service/impl/ZoneConfigEventServiceImpl.java
@@ -3,22 +3,31 @@ package co.ud.hashticket.datos.service.impl;
 import co.ud.hashticket.datos.entity.ZoneConfigEventEntity;
 import co.ud.hashticket.datos.repository.ZoneConfigEventRepository;
 import co.ud.hashticket.datos.service.ZoneConfigEventService;
+import co.ud.ud.hashticket.exception.BusinessException;
+import co.ud.ud.hashticket.exception.enumeration.TYPE_EXCEPTION;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.BiPredicate;
 
+@Slf4j
 @Service
 public class ZoneConfigEventServiceImpl implements ZoneConfigEventService {
     private ZoneConfigEventRepository zoneConfigEventRepository;
+    private BiPredicate<Long,Long> isZoneConfigEventExists = (zoneId, configEventd) -> zoneConfigEventRepository.getByZoneAndConfigEvent(zoneId,configEventd).isPresent();
     @Autowired
     public ZoneConfigEventServiceImpl(ZoneConfigEventRepository zoneConfigEventRepository) {
         this.zoneConfigEventRepository = zoneConfigEventRepository;
     }
     @Override
     public ZoneConfigEventEntity save(ZoneConfigEventEntity zoneConfigEvent) {
+        if(isZoneConfigEventExists.test(zoneConfigEvent.getZone().getId(), zoneConfigEvent.getConfigEvent().getId())){
+            throw new BusinessException(1L, TYPE_EXCEPTION.ERROR, "Configuración existente");
+        }
         return zoneConfigEventRepository.save(zoneConfigEvent);
     }
     @Override
